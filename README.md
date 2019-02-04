959d62becf77bed7a0245c6814f37b8e2b99a14a
Theo <theo.despoudis@teckro.com>
2019-02-04 21:37:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+go-leetcode
+---
+Solutions and implementations of Leetcode problems in Go.
+May the code be with you.
+
+## Challenges completed
+
+- [] **Tries**: [Link](https://leetcode.com/explore/learn/card/trie/150/introduction-to-trie/)
+    - [x] : Trie Representation
+    - [] : Trie Operations
+
+## LICENCE
+Copyright © 2019 Theo Despoudis MIT license
---
+++ trie/node.go
@@ -0,0 +1,25 @@
+package trie
+
+const Cases = 26
+
+// Waste of space
+type ArrayTrieNode struct {
+	children *[Cases]ArrayTrieNode
+}
+
+func NewArrayTrieNode() *ArrayTrieNode  {
+	return &ArrayTrieNode{
+		children: &[Cases]ArrayTrieNode{},
+	}
+}
+
+// More flexible
+type MapTrieNode struct {
+	children map[rune]MapTrieNode
+}
+
+func NewMapTrieNode() *MapTrieNode  {
+	return &MapTrieNode{
+		children: make(map[rune]MapTrieNode),
+	}
+}
